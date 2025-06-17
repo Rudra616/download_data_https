40e764f91461ad25694c889a435574ec8fee9838
rudra <rudrampanchal@gmail.com>
2025-06-17 09:06:44 +0530

    connect model and html page and store the data and print in terminal

--- app/views.py
+++ app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from .models import form
 # Create your views here.
+def form(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        address = request.POST.get('address')
+        print(f"Name: {name}, Email: {email}, Phone: {phone}, Address: {address}")
+        return render(request,'form.html', {'message': 'Submitted successfully!'})
+
+        
+    return render(request,'form.html')

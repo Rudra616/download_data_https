13bfe8c7c55e7c1ddd7317e529a4916d42ad89e2
rudra <rudrampanchal@gmail.com>
2025-06-17 09:02:59 +0530

    create form(name,email,mobilenum,addres)

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class form(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    mob = models.TextField()
+    addres = models.TextField()
+
+    
+    def __str__(self):
+        return self.name
